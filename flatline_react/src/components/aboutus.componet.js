43b33f073ff49e93aaef6f5477819041e3077839
Terry Carpenter <terry.carpenter@outlook.com>
2022-11-15 19:31:51 -0600

    first commit

---
+++ flatline_react/src/components/aboutus.componet.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+export default class AboutUs extends Component {
+  render() {
+    return (
+      <form>
+        <h3>About Flatline Robotics</h3>
+  
+<p>
+
+</p>
+      </form>
+    )
+  }
+}
---
+++ flatline_react/src/components/main.componet.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+export default class Main extends Component {
+  render() {
+    return (
+      <form>
+        <h3>Main page for img and info</h3>
+  
+<p>
+
+</p>
+      </form>
+    )
+  }
+}
